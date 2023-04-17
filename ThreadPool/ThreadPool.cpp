504ca67548130f5b85535d3399ee7300c41e6a8c
Xinglong Geng <320188900@qq.com>
2023-04-17 10:57:11 +0800

    Add an achievement of thread pool in C++

---
+++ ThreadPool/ThreadPool.cpp
@@ -0,0 +1,72 @@
+﻿#include "threadpool.h"
+#include <random>
+#include <iostream>
+
+std::random_device rd;  //true random number generator
+std::mt19937 mt(rd());   //to generate calculated random number
+std::uniform_int_distribution<int> dist(-1000, 1000);
+auto rnd = std::bind(dist, mt);
+
+void simulate_hard_computation()
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000 + rnd()));
+}
+
+void multiply(const int a, const int b)
+{
+	simulate_hard_computation();
+	const int res = a * b;
+	std::cout << a << " * " << b << " = " << res << std::endl;
+}
+
+void multiply_output(int &out, const int a, const int b)
+{
+	simulate_hard_computation();
+	out = a * b;
+	std::cout << a << " * " << b << " = " << out << std::endl;
+}
+
+int multiply_return(const int a, const int b)
+{
+	simulate_hard_computation();
+	const int res = a * b;
+	std::cout << a << " * " << b << " = " << res << std::endl;
+	return res;
+}
+
+void example()
+{
+	ThreadPool pool(3);    //to create 3 thread pool
+	pool.init();
+
+	//to submit multiply operator, totally 30
+	for (int i = 1; i <= 3; ++i)
+		for (int j = 1; j <= 10; ++j)
+			pool.submit(multiply, i, j);
+
+	//use the output value refered by ref to submit function
+	int output_ref;
+	auto future1 = pool.submit(multiply_output, std::ref(output_ref), 5, 6);
+
+	//wait for multiply_output to finish
+	future1.wait();
+	std::cout << "Last operation result is equals to " << output_ref << std::endl;
+
+	//to use return value to submit function
+	auto future2 = pool.submit(multiply_return, 5, 3);
+
+	//wait for multiply_return function to finish
+	int res = future2.get();
+	std::cout << "Last operation result is equals to " << res << std::endl;
+
+	//to shut down thread pool
+	pool.shutdown();
+}
+
+int main()
+{
+	example();
+	
+	return 0;
+}
+
---
+++ ThreadPool/threadpool.h
@@ -0,0 +1,151 @@
+﻿#pragma once
+
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <mutex>
+#include <queue>
+#include <functional>
+#include <future>
+#include <thread>
+#include <utility>
+#include <vector>
+#include <condition_variable>
+
+//a safe task queue
+template <typename T>
+class SafeQueue
+{
+private:
+	std::queue<T> m_queue;
+	std::mutex m_mutex;
+
+public:
+	SafeQueue(){}
+	SafeQueue(SafeQueue &&other){}
+	~SafeQueue(){}
+
+	bool empty()
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+
+		return m_queue.empty();
+	}
+
+	int size()
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+
+		return m_queue.size();
+	}
+
+	void enqueue(T& t)
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+		m_queue.emplace(t);
+	}
+
+	bool dequeue(T& t)
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+
+		if (m_queue.empty)
+			return false;
+		t = std::move(m_queue.front());
+
+		m_queue.pop();
+		return true;
+	}
+};
+
+//a thread pool used to run the task cyclely
+class ThreadPool
+{
+private:
+	class ThreadWorker
+	{
+	private:
+		int m_id;  //thread id
+		ThreadPool* m_pool;
+
+	public:
+		//constructor of ThreadWorker
+		ThreadWorker(ThreadPool *pool, const int id):m_pool(pool), m_id(id)
+		{}
+
+		// to overload the () function
+		void operator()()
+		{
+			std::function<void()> func;
+			bool dequeued;
+
+			//to constantly run the task in the task queue, wait when the task queue is empty, 
+			//and wake when task-adding action happens in the task queue
+		    while (!m_pool->m_shutdown)
+			{
+				{
+					std::unique_lock<std::mutex> lock(m_pool->m_condition_mutex);
+
+					if (m_pool->m_queue.empty())
+						m_pool->m_conditional_lock.wait(lock);
+
+					dequeued = m_pool->m_queue.dequeue(func);
+				}
+				if (dequeued)
+					func();
+			}
+		}
+	};
+	bool m_shutdown;
+	SafeQueue<std::function<void()>> m_queue;
+	std::vector<std::thread> m_threads;
+	std::mutex m_condition_mutex;
+	std::condition_variable m_conditional_lock;
+
+public:
+	//contructor of ThreadPool
+	ThreadPool(const int n_threads = 4)    //n_threads represents the max amount of threads in the thread pool
+		:m_threads(std::vector<std::thread>(n_threads)), m_shutdown(false)
+	{}
+	
+	ThreadPool(const ThreadPool&) = delete;
+	ThreadPool(ThreadPool&&) = delete;
+	ThreadPool& operator=(const ThreadPool&) = delete;
+	ThreadPool& operator=(ThreadPool&&) = delete;
+
+	//to initialize the thread pool
+	void init()
+	{
+		for (unsigned int i = 0; i < m_threads.size(); ++i)
+			m_threads.at(i) = std::thread(ThreadWorker(this, i));
+	}
+
+	//to shut down the thread pool
+	void shutdown()
+	{
+		m_shutdown = true;
+		m_conditional_lock.notify_all();
+
+		for (unsigned int i = 0; i < m_threads.size(); ++i)
+			if (m_threads.at(i).joinable())
+				m_threads.at(i).join();
+	}
+
+	template<typename F, typename... Args>
+	auto submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))>
+	{
+		std::function<decltype(f(args...))()> func = std::bind(std::forward<F>f, std::forward<Args>(args...));  // something wrong happens here.
+
+		auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+
+		std::function<void()> warpper_func = [task_ptr]()
+		{
+			(*task_ptr)();
+		};
+
+		m_queue.enqueue(wrapper_func);
+		m_conditional_lock.notify_one();
+		return task_ptr->get_future();
+	}
+};
+#endif
